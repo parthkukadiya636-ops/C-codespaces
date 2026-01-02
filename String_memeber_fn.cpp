10985ae9f6de66ee31c1d6cb9e2d7762f7d1e44d
Parth kukadiya <parthkukadiya636@gmail.com>
2026-01-02 20:36:38 +0530

    C++

---
+++ String_memeber_fn.cpp
@@ -0,0 +1,15 @@
+// str.lenght()-> use to calculate the length of any functon.
+
+// str.at(inx) -> gives the value or charcater at that string.
+
+// str.substr(startidx, Size)-> gives the substring from starting index to the size given.
+
+// str.find(word) -> find the word if present in the string else it will return -1.
+
+// dynamic memory allocation :
+ 
+int size;
+cin>>size;
+
+int *ptr = new int[size];
+
